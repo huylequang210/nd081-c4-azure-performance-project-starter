705cc7e6fbceec97c2e1bbd387e7548f698a0ebd
huylequang210 <huylequanq210@gmail.com>
2023-05-27 20:28:49 +0700

    change image

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -85,7 +85,17 @@ if "TITLE" in os.environ and os.environ["TITLE"]:
 else:
     title = app.config["TITLE"]
 
-r = redis.Redis()
+redis_server = os.environ['REDIS']
+try:
+    if "REDIS_PWD" in os.environ:
+        r = redis.StrictRedis(host=redis_server,
+                        port=6379,
+                        password=os.environ['REDIS_PWD'])
+    else:
+        r = redis.Redis(redis_server)
+    r.ping()
+except redis.ConnectionError:
+    exit('Failed to connect to Redis, terminating.')
 
 # Change title to host name to demo NLB
 if app.config["SHOWHOST"] == "true":
